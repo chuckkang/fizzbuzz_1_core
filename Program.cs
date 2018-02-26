b3a69df5162451e9b84ca6103fc612832fe7794c
Chuck Kang <chuckkang@gmail.com>
2018-02-26 11:24:48 -0800

    fizz buzz

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace first_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+			for (int x = 1; x <= 100; x++)
+			{
+				if (x%15==0){
+					Console.WriteLine(x + "--Fizz Buzz");
+				} else if (x%5==0) {
+					Console.WriteLine(x + "--fizz");
+				} else if ( x % 3 == 0) {
+					Console.WriteLine(x + " - buzz");
+				} else {
+					Console.WriteLine(x);
+				}
+			}
         }
+
     }
 }
